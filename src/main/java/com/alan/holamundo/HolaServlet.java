73b9718604b463b015749224ed57c38e737c9798
Alan <alansvela2019@gmail.com>
2025-05-07 09:19:07 -0500

    inicioTomcat2

--- src/main/java/com/alan/holamundo/HolaServlet.java
+++ src/main/java/com/alan/holamundo/HolaServlet.java
@@ -7,12 +7,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 //necesitamos implementar una llave
 @WebServlet("/holaServlet")
 public class HolaServlet extends HttpServlet {
-    @Override
+    @Override//sobreescribimos el metodo doget
+    //manejo de excepciones, request es el mensaje y manejo de usuario, resp es la respuesta de la apliacion, ponemos excepciones paar posibles errores
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-
+    resp.setContentType("text/html");
+        //creamos un objeto de tipo Print Write, esto me va apermitir escribir en el navegador
+        PrintWriter out = resp.getWriter();
+    //generamos el contenido de respuesta
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>Hola Servlet</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>Hola Servlet</h1>");
+    out.println("</body>");
+    out.println("</html>");
+    out.close();
     }
 }
